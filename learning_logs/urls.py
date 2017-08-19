288cb25552f6b8386277a5f9ce354fd31d8fc59e
Roman <roma2@protonmail.com>
2017-08-19 18:57:06 +0300

    Add Topic detail page - URL, views, template

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,4 +7,5 @@ app_name = 'learning_logs'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'), 
     url(r'^topics/$', views.TopicList.as_view(), name='topics'),
+    url(r'^topics/(?P<topic_slug>[\w-]+)/$', views.TopicDetail.as_view(), name='topic'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import TemplateView, ListView, DetailView
 
 from .models import Topic, Entry
 
@@ -11,3 +11,19 @@ class TopicList(ListView):
     model = Topic
     template_name = 'learning_logs/topics.html' 
     context_object_name = 'topics'
+
+
+class TopicDetail(ListView):
+
+    template_name = 'learning_logs/topic.html'
+    
+    def get_queryset(self):
+        self.topic = get_object_or_404(Topic, slug=self.kwargs['topic_slug'])
+        return Entry.objects.filter(topic=self.topic)
+    
+    def get_context_data(self, **kwargs):
+        context = super(TopicDetail, self).get_context_data(**kwargs)
+        context['topic'] = self.topic
+        return context
+
+    context_object_name = 'entries'
