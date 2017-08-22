53e0a9861856063ca3d06d63456a1e5ccb427a11
Roman <roma2@protonmail.com>
2017-08-22 10:49:22 +0300

    Add entry create and entry update pages

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -6,12 +6,13 @@ app_name = 'learning_logs'
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'), 
-    url(r'^topic/list/$', views.TopicList.as_view(), name='topics'),
-    url(r'^topic/create/$', views.TopicCreate.as_view(), name='topic_create'),
-    url(r'^topic/(?P<topic_slug>[\w-]+)/update/$', views.TopicUpdate.as_view(), name='topic_update'),
-    url(r'^topic/(?P<topic_slug>[\w-]+)/delete/$', views.TopicDelete.as_view(), name='topic_delete'),
-    url(r'^topic/(?P<topic_slug>[\w-]+)/entry/list/$', views.TopicDetail.as_view(), name='topic'),
-    url(r'^topic/(?P<topic_slug>[\w-]+)/entry/create/$', views.EntryCreate.as_view(), name='entry_create'),
-    url(r'^topic/(?P<topic_slug>[\w-]+)/entry/(?P<entry_slug>[\w-]+)/$',
-        views.EntryDetail.as_view(), name='entry'),
+    url(r'^topics/$', views.TopicList.as_view(), name='topics'),
+    url(r'^topic-add/$', views.TopicCreate.as_view(), name='topic_create'),
+    url(r'^topic/(?P<slug>[\w-]+)/update/$', views.TopicUpdate.as_view(), name='topic_update'),
+    url(r'^topic/(?P<slug>[\w-]+)/delete/$', views.TopicDelete.as_view(), name='topic_delete'),
+    url(r'^topic/(?P<slug>[\w-]+)/$', views.TopicDetail.as_view(), name='topic'),
+    url(r'^topic/(?P<slug>[\w-]+)/entry-add/$', views.EntryCreate.as_view(), name='entry_create'),
+    url(r'^entry/(?P<slug>[\w-]+)/$', views.EntryDetail.as_view(), name='entry'),
+    url(r'^entry/(?P<slug>[\w-]+)/update/$', views.EntryUpdate.as_view(), name='entry_update'),
+    url(r'^entry/(?P<slug>[\w-]+)/delete/$', views.EntryDelete.as_view(), name='entry_delete'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import TemplateView, ListView, DetailView
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import View, TemplateView, ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
+from django.urls import reverse, reverse_lazy
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -12,17 +12,15 @@ class IndexView(TemplateView):
 
 
 class TopicList(ListView):
-    model = Topic
+    queryset = Topic.objects.order_by('title')
     template_name = 'learning_logs/topics.html' 
     context_object_name = 'topics'
 
 
 class TopicDetail(ListView):
 
-    template_name = 'learning_logs/topic.html'
-    
     def get_queryset(self):
-        self.topic = get_object_or_404(Topic, slug=self.kwargs['topic_slug'])
+        self.topic = get_object_or_404(Topic, slug=self.kwargs['slug'])
         return Entry.objects.filter(topic=self.topic).order_by('-date_added')
     
     def get_context_data(self, **kwargs):
@@ -30,12 +28,13 @@ class TopicDetail(ListView):
         context['topic'] = self.topic
         return context
 
+    template_name = 'learning_logs/topic.html'
     context_object_name = 'entries'
 
 
 class EntryDetail(ListView):
     def get_queryset(self):
-        self.entry = get_object_or_404(Entry, slug=self.kwargs['entry_slug'])
+        self.entry = get_object_or_404(Entry, slug=self.kwargs['slug'])
         return self.entry
 
     context_object_name = 'entry'
@@ -43,7 +42,6 @@ class EntryDetail(ListView):
     
 
 class TopicCreate(CreateView):
-    model = Topic
     form_class = TopicForm
     template_name = 'learning_logs/topic_create.html'
     success_url = reverse_lazy('learning_logs:topics')
@@ -51,7 +49,6 @@ class TopicCreate(CreateView):
 
 class TopicUpdate(UpdateView):
     model = Topic
-    slug_url_kwarg = 'topic_slug'
     form_class = TopicForm
     context_object_name = 'topic'
     template_name = 'learning_logs/topic_update.html'
@@ -60,9 +57,57 @@ class TopicUpdate(UpdateView):
 
 class TopicDelete(DeleteView):
     model = Topic
-    slug_url_kwarg = 'topic_slug'
     success_url = reverse_lazy('learning_logs:topics')
     
 
-class EntryCreate(CreateView):
+class EntryCreate(View):
+    form_class = EntryForm
+    template_name = 'learning_logs/entry_create.html'
+
+    def get(self, request, *args, **kwargs):
+        topic = get_object_or_404(Topic, slug=self.kwargs['slug'])
+        form = self.form_class()
+        return render(request, self.template_name, {'topic': topic, 'form': form})
+
+    def post(self, request, *args, **kwargs):
+        topic = get_object_or_404(Topic, slug=self.kwargs['slug'])
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic.slug)
+
+        return render(request, self.template_name, {'topic': topic, 'form': form})
+
+
+class EntryUpdate(View):
+    form_class = EntryForm
+    template_name = 'learning_logs/entry_update.html'
+
+    def get(self, request, *args, **kwargs):
+        entry = get_object_or_404(Entry, slug=self.kwargs['slug'])
+        topic = entry.topic
+        form = self.form_class(instance=entry)
+        return render(request, self.template_name, {
+            'entry': entry,
+            'topic': topic,
+            'form': form,
+        })
+
+    def post(self, request, *args, **kwargs):
+        entry = get_object_or_404(Entry, slug=self.kwargs['slug'])
+        topic = entry.topic
+        form = self.form_class(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:entry', entry.slug)
+        return render(request, self.template_name, {
+            'entry': entry,
+            'topic': topic,
+            'form': form,
+        })
+
+
+class EntryDelete(View):
     pass
