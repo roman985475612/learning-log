0421fc7404ae27706e8d9480d424eb81db60dcd7
Roman <roma2@protonmail.com>
2017-09-29 14:32:55 +0300

    Added a link to the entree's owner on the profile page

--- users/urls.py
+++ users/urls.py
@@ -1,23 +1,14 @@
 from django.conf.urls import url
 
-from .views import (
-    LoginView,
-    LogoutView,
-    RegisterView,
-    PersonDetailView,
-    PersonCreateView,
-    PersonUpdateView,
-    PersonDeleteView,
-)
+from . import views
 
 app_name = 'users'
 
 urlpatterns = [
-    url(r'^login/$', LoginView.as_view(), name='login'),
-    url(r'^logout/$', LogoutView.as_view(), name='logout'),
-    url(r'^register/$', RegisterView.as_view(), name='register'),
-    url(r'^profile/$', PersonDetailView.as_view(), name='profile'),
-    url(r'^profile/create/$', PersonCreateView.as_view(), name='profile_create'),
-    url(r'^profile/update/$', PersonUpdateView.as_view(), name='profile_update'),
-    url(r'^profile/delete/$', PersonDeleteView.as_view(), name='profile_delete'),
+    url(r'^login/$', views.LoginView.as_view(), name='login'),
+    url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
+    url(r'^register/$', views.RegisterView.as_view(), name='register'),
+    url(r'^profile/(?P<pk>\d+)/$', views.PersonDetailView.as_view(), name='profile'),
+    url(r'^profile/(?P<pk>\d+)/create/$', views.PersonCreateView.as_view(), name='profile_create'),
+    url(r'^profile/(?P<pk>\d+)/update/$', views.PersonUpdateView.as_view(), name='profile_update'),
 ]
--- users/views.py
+++ users/views.py
@@ -4,13 +4,22 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import Http404
 from django.views.generic.base import TemplateView, View
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, DeleteView, FormView
+from django.views.generic.edit import CreateView, DeleteView, FormView, UpdateView
 from django.views.generic.list import ListView
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse, reverse_lazy
 
 from .models import Person
 from .forms import PersonForm
+
+
+class OwnerVerificationMixins:
+
+    def get_object(self):
+        object = super().get_object()
+        if object.owner != self.request.user:
+            raise Http404("You are not the owner")
+        return object
 
 
 class LoginView(FormView):
@@ -44,31 +53,13 @@ class RegisterView(FormView):
         return redirect(self.success_url)
 
 
-class PersonMixins(View):
-    def get_person(self, request, *args, **kwargs):
-        self.person = Person.objects.get(owner=self.request.user)
-        return self.person
-
-    def dispatch(self, request, *args, **kwargs):
-        try:
-            person = self.get_person(request)
-        except Person.DoesNotExist:
-            return redirect('users:profile_create')
-        return super(PersonMixins, self).dispatch(request)
-
-
-class PersonDetailView(LoginRequiredMixin, PersonMixins, TemplateView):
+class PersonDetailView(LoginRequiredMixin, DetailView):
+    model = Person
     template_name = 'users/person_detail.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['person'] = self.get_person(self.request)
-        context['entry_list'] = context['person'].owner.entry.all()
-        context['comment_list'] = context['person'].owner.comment.order_by('entry', '-date_added')
-        return context
+    context_object_name = 'person'
 
 
-class PersonCreateView(LoginRequiredMixin, CreateView):
+class PersonCreateView(LoginRequiredMixin, OwnerVerificationMixins, CreateView):
     model = Person
     form_class = PersonForm
     template_name_suffix = '_create'
@@ -79,28 +70,14 @@ class PersonCreateView(LoginRequiredMixin, CreateView):
         return super(PersonCreateView, self).form_valid(form)
 
 
-class PersonUpdateView(LoginRequiredMixin, PersonMixins, FormView):
+class PersonUpdateView(LoginRequiredMixin, OwnerVerificationMixins, UpdateView):
+    model = Person
     form_class = PersonForm
-    template_name = 'users/person_update.html'
-    success_url = reverse_lazy('users:profile')
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        person = self.get_person(self.request)
-        context['form'] = self.form_class(instance=person)
-        return context
+    template_name_suffix = '_update'
 
     def form_valid(self, form):
-        person = Person.objects.get(owner=self.request.user)
-        form = self.form_class(instance=person, data=self.request.POST)
         form.save()
         return super().form_valid(form)
 
-
-class PersonDeleteView(LoginRequiredMixin, PersonMixins, TemplateView):
-    template_name = 'users/person_delete.html'
-
-    def post(self, request, *args, **kwargs):
-        person = self.get_person(request)
-        person.delete()
-        return redirect('learning_logs:index')
+    def get_success_url(self):
+        return reverse('users:profile', kwargs={'pk': self.object.pk})
