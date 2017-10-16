5259d552038469fe36e2602f2dcd59896981fa02
Roman <roma2@protonmail.com>
2017-10-16 11:32:01 +0300

    Task # 22 - Migrated to the PostgreSQL

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'learning_log.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'livejournal',
+        'USER': 'lj',
+        'PASSWORD': 'ffurEL1hl2',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
