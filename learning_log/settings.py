f9b8d16d91755aceaed16bafaefbe8fb8d89bffc
Roman <roma2@protonmail.com>
2017-12-10 17:59:50 +0300

    Set DEBUG=False on Heroku

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -155,7 +155,7 @@ if cwd == '/app' or cwd[:4] == '/tmp':
 
     # Allow all host headers.
     ALLOWED_HOSTS = ['livejournal.herokuapp.com']
-    DEBUG = True
+    DEBUG = False
 
     # Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
