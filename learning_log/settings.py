049af24850ce99e6817e2e935ad432de56e6fcb6
Roman <roma2@protonmail.com>
2017-12-15 13:42:01 +0300

    Changed settings: added STATICFILES_DIRS

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -161,3 +161,4 @@ if cwd == '/app' or cwd[:4] == '/tmp':
     # Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'learning_log/static')]
