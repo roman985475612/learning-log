053c3c920021f5bfc1eba7840ea78b517f467477
Roman <roma2@protonmail.com>
2017-12-15 14:38:05 +0300

    Modified: learning_log/settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a3t2^t4!(qza5kh4@vu8-($ueopim%sd04$w^d2rbtk0cl8hrz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
